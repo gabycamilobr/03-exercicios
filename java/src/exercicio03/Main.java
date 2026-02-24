d876b342cc8ae3f02ec2ad637754824421822c92
Gabrielly Camilo <gabycamilobr@gmail.com>
2026-02-24 20:52:55 -0300

    Terceiro exercicio finalizado

--- java/src/exercicio03/Main.java
+++ java/src/exercicio03/Main.java
@@ -1,4 +1,15 @@
 package exercicio03;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Professor professor = new Professor();
+
+        professor.totalDeAulas = 40;
+        professor.valorDaAula = 220;
+
+        System.out.println(professor.calcularSalario());
+    }
 }
--- java/src/exercicio03/Professor.java
+++ java/src/exercicio03/Professor.java
@@ -4,4 +4,16 @@ public class Professor {
     String nome;
     int totalDeAulas;
     double valorDaAula;
+
+    public double calcularSalario() {
+        double salarioBase;
+        double horaAtividade;
+        double descansoSemanal;
+
+        salarioBase = totalDeAulas * valorDaAula * 4.5;
+        horaAtividade = salarioBase * 5 / 100;
+        descansoSemanal = (salarioBase + horaAtividade) / 6;
+
+        return salarioBase + horaAtividade + descansoSemanal;
+    }
 }
